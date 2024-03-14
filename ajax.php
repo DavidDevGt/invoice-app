363172769d33f387253ee05ee9d7a050044a1bbb
DavidDevGt <josuedavidvl18@gmail.com>
2024-03-14 14:31:19 -0600

    Config php poo env

--- ajax.php
+++ ajax.php
@@ -1,10 +1,10 @@
 <?php
 
 session_start();
-include './config/db.php'; // Asegúrate de que este archivo contenga la función getDbConnection()
+include './src/config/db.php';
 
 if (isset($_SESSION['usuario_id'])) {
-    header('Location: modules/dashboard.php'); // Redirige al usuario a la página de inicio si ya inició sesión
+    header('Location: modules/dashboard.php');
     exit;
 }
 
@@ -14,7 +14,7 @@ if (isset($_POST['fnc']) && $_POST['fnc'] == "login") {
 
     if ($usuario !== '' && $password !== '') {
         $conn = getDbConnection();
-        // Modifica la consulta para obtener el hash de la contraseña
+
         $stmt = $conn->prepare("SELECT id, nombre, email, password, rol_id FROM usuarios WHERE usuario = ? AND active = TRUE");
         $stmt->bind_param("s", $usuario);
         $stmt->execute();
@@ -22,7 +22,7 @@ if (isset($_POST['fnc']) && $_POST['fnc'] == "login") {
         $user = $result->fetch_assoc();
 
         if ($user && password_verify($password, $user['password'])) {
-            // La contraseña coincide
+
             $_SESSION['usuario_id'] = $user['id'];
             $_SESSION['nombre'] = $user['nombre'];
             $_SESSION['email'] = $user['email'];
@@ -30,7 +30,7 @@ if (isset($_POST['fnc']) && $_POST['fnc'] == "login") {
         
             echo json_encode(['success' => true]);
         } else {
-            // Usuario no encontrado o contraseña incorrecta
+
             echo json_encode(['success' => false]);
         }
         $stmt->close();
