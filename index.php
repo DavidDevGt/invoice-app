43d03918867bca9880ed259259e1262a6f2d20f3
DavidDevGt <josuedavidvl18@gmail.com>
2024-03-14 12:59:43 -0600

    arreglos menores login

--- index.php
+++ index.php
@@ -5,33 +5,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | Invoice2Go</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="src/login-style.css">
 </head>
 
 <body>
     <main class="container">
-    <div class="row justify-content-center">
+        <div class="row justify-content-center">
             <div class="col-md-4 login-container">
                 <h1 class="text-center mb-4">RRHH Adminer</h1>
                 <form id="loginForm">
                     <div class="form-group">
-                        <label for="username">Usuario</label>
+                        <label class="mb-2" for="username">Usuario</label>
                         <input type="text" class="form-control" id="username" name="username" required>
                     </div>
                     <div class="form-group position-relative mt-3">
-                        <label for="password">Contraseña</label>
+                        <label class="mb-2" for="password">Contraseña</label>
                         <input type="password" class="form-control" id="password" name="password" required>
                         <span toggle="#password" class="fa fa-fw fa-eye field-icon toggle-password"></span>
                     </div>
-                    <button type="submit" class="btn btn-primary btn-block mt-3">Iniciar Sesión</button>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary btn-block mt-3">Iniciar Sesión</button>
+                    </div>
                 </form>
             </div>
         </div>
     </main>
     <script src="app.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </body>
 
