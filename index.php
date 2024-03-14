d51d12493a42e87c416ba9ef1b3cae482feb4d2b
DavidDevGt <josuedavidvl18@gmail.com>
2024-03-14 11:12:20 -0600

    bug fixing frontend login view

--- index.php
+++ index.php
@@ -7,24 +7,25 @@
     <title>Login | Invoice2Go</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="src/login-style.css">
 </head>
 
 <body>
     <main class="container">
-        <h1 class="text-center">Bienvenido al Sistema de RRHH</h1>
-        <div class="row justify-content-center">
-            <div class="col-md-6">
+    <div class="row justify-content-center">
+            <div class="col-md-4 login-container">
+                <h1 class="text-center mb-4">RRHH Adminer</h1>
                 <form id="loginForm">
                     <div class="form-group">
-                        <label for="username">Nombre de Usuario</label>
+                        <label for="username">Usuario</label>
                         <input type="text" class="form-control" id="username" name="username" required>
                     </div>
-                    <div class="form-group">
+                    <div class="form-group position-relative">
                         <label for="password">Contraseña</label>
                         <input type="password" class="form-control" id="password" name="password" required>
                         <span toggle="#password" class="fa fa-fw fa-eye field-icon toggle-password"></span>
                     </div>
-                    <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
+                    <button type="submit" class="btn btn-primary btn-block mt-3">Iniciar Sesión</button>
                 </form>
             </div>
         </div>
