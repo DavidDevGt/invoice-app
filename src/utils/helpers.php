4e9181505551874412373bc87029cc599aadcb97
DavidDevGt <josuedavidvl18@gmail.com>
2024-04-02 18:27:40 -0600

    agregar funciones y libs composer

--- src/utils/helpers.php
+++ src/utils/helpers.php
@@ -1,9 +1,5 @@
 <?php
-
-function formatDate($date, $format = 'Y-m-d H:i:s') {
-    return date($format, strtotime($date));
-}
-
+//*** START - Security ***// 
 function sanitizeInput($data) {
     $data = trim($data);
     $data = stripslashes($data);
@@ -11,6 +7,22 @@ function sanitizeInput($data) {
     return $data;
 }
 
+function tokenGenerator($length = 50) {
+    return bin2hex(random_bytes($length));
+}
+
+// Esto para sanitizar un array de datos
+function sanitizeInputArray($inputArray) {
+    $sanitizedArray = [];
+    foreach ($inputArray as $key => $value) {
+        $sanitizedArray[$key] = sanitizeInput($value);
+    }
+    return $sanitizedArray;
+}
+
+//*** END - Security ***// 
+
+//*** START - AJAX Functions ***//
 function jsonResponse($status, $message, $data = null) {
     header('Content-Type: application/json');
     echo json_encode([
@@ -20,3 +32,26 @@ function jsonResponse($status, $message, $data = null) {
     ]);
     exit();
 }
+
+//*** END - AJAX Functions ***//
+
+//*** START - Date Format ***//
+function diffDate($start_date, $end_date) {
+    $start = new DateTime($start_date);
+    $end = new DateTime($end_date);
+    $diff = $start->diff($end);
+    return $diff->days;
+}
+
+function formatDate($date, $format = 'Y-m-d H:i:s') {
+    return date($format, strtotime($date));
+}
+
+function calculateAge($birthDate) {
+    $today = new DateTime();
+    $birthdate = new DateTime($birthDate);
+    $age = $today->diff($birthdate);
+    return $age->y;
+}
+
+//*** END - Date Format ***// 
