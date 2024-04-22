897aae1edb06f7ccebb13c6c66a16eb6c7f09a13
DavidDevGt <josuedavidvl18@gmail.com>
2024-04-22 05:48:28 -0600

    dockerizando proyecto

--- src/utils/helpers.php
+++ src/utils/helpers.php
@@ -1,18 +1,21 @@
 <?php
 //*** START - Security ***// 
-function sanitizeInput($data) {
+function sanitizeInput($data): string
+{
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
 }
 
-function tokenGenerator($length = 50) {
+function tokenGenerator($length = 50): string
+{
     return bin2hex(random_bytes($length));
 }
 
 // Esto para sanitizar un array de datos
-function sanitizeInputArray($inputArray) {
+function sanitizeInputArray($inputArray): array
+{
     $sanitizedArray = [];
     foreach ($inputArray as $key => $value) {
         $sanitizedArray[$key] = sanitizeInput($value);
@@ -36,18 +39,21 @@ function jsonResponse($status, $message, $data = null) {
 //*** END - AJAX Functions ***//
 
 //*** START - Date Format ***//
-function diffDate($start_date, $end_date) {
+function diffDate($start_date, $end_date): bool|int
+{
     $start = new DateTime($start_date);
     $end = new DateTime($end_date);
     $diff = $start->diff($end);
     return $diff->days;
 }
 
-function formatDate($date, $format = 'Y-m-d H:i:s') {
+function formatDate($date, $format = 'Y-m-d H:i:s'): string
+{
     return date($format, strtotime($date));
 }
 
-function calculateAge($birthDate) {
+function calculateAge($birthDate): int
+{
     $today = new DateTime();
     $birthdate = new DateTime($birthDate);
     $age = $today->diff($birthdate);
